672c1e11e36555d50a29e5dd381ae13cc96598a3
Daniel de Souza Baule <dsbaule@gmail.com>
2015-05-22 17:33:57 -0300

    V05 - Exercicio 5 feito
    Ajustados erros anteriores

--- Estruturas.c
+++ Estruturas.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+void leInstancias(struct Ponto2D *p, struct Circulo *c)
+{
+	printf("\nDigite o valor de x do Ponto: ");
+	scanf("%f", p.x);
+	printf("\nDigite o valor de y do Ponto: ");
+	scanf("%f", p.y);
+	
+	printf("\n");
+	
+	printf("\nAgora digite o valor de x do centro do circulo: ");
+	scanf("%f", c.centro.x);
+	printf("\nAgora digite o valor de y do centro do circulo: ");
+	scanf("%f", c.centro.y);
+	printf("\nAgora digite o valor do raio do circulo: ");
+	scanf("%f", c.raio);
+	printf("\nAgora digite o valor de cor R para o circulo: ");
+	scanf("%f", c.cor.r);
+	printf("\nAgora digite o valor de cor G para o circulo: ");
+	scanf("%f", c.cor.g);
+	printf("\nAgora digite o valor de cor B para o circulo: ");
+	scanf("%f", c.cor.b);
+}
+
 int main()
 {
 	struct Ponto2D
@@ -17,9 +40,9 @@ int main()
 	
 	struct Circulo
 	{
-		struct 	Ponto2D centro;
+		struct 	Ponto2D	centro;
 		float 	raio;
-		float 	cor;
+		struct 	CorRGB	cor;
 	};
 	
 	struct Ponto2D ponto1;
