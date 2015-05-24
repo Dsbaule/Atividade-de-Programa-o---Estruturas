bcc190d00866ec480a187af45be1231d294da75c
Daniel de Souza Baulé <dsbaule@gmail.com>
2015-05-24 20:37:34 -0300

    V07 - Exercicio 7 feito
    Consertados erros anteriores causados pela reversão de versão pelo
    GitHub

--- Estruturas.c
+++ Estruturas.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 struct Ponto2D
 {
@@ -24,30 +25,21 @@ void leInstancias(struct Ponto2D *p, struct Circulo *c)
 {
     printf("\nDigite o valor de x do Ponto: ");
     scanf("%f", &p->x);
-    scanf("%f", p.x);
     printf("\nDigite o valor de y do Ponto: ");
     scanf("%f", &p->y);
 
-    scanf("%f", p.y);
-
     printf("\n");
-
 
     printf("\nAgora digite o valor de x do centro do circulo: ");
     scanf("%f", &c->centro.x);
-    scanf("%f", c.centro.x);
     printf("\nAgora digite o valor de y do centro do circulo: ");
     scanf("%f", &c->centro.y);
-    scanf("%f", c.centro.y);
     printf("\nAgora digite o valor do raio do circulo: ");
     scanf("%f", &c->raio);
-    scanf("%f", c.raio);
     printf("\nAgora digite o valor de cor R para o circulo: ");
     scanf("%d", &c->cor.r);
-    scanf("%f", c.cor.r);
     printf("\nAgora digite o valor de cor G para o circulo: ");
     scanf("%d", &c->cor.g);
-    scanf("%f", c.cor.g);
     printf("\nAgora digite o valor de cor B para o circulo: ");
     scanf("%d", &c->cor.b);
 }
@@ -56,7 +48,18 @@ void mostraInstancias(const struct Ponto2D *p, const struct Circulo *c)
 {
     printf("Ponto:\n\tX - %.2f\n\tY - %.2f\n\n", p->x, p->y);
     printf("Circulo:\n\tX - %.2f\n\tY - %.2f\n\tRaio - %.2f\n\tCor R - %d\n\tCor G - %d\n\tCor B - %d\n\n", c->centro.x, c->centro.y, c->raio, c->cor.r, c->cor.g, c->cor.b);
-    scanf("%f", c.cor.b);
+}
+
+int ehInterno(const struct Ponto2D *p, const struct Circulo *c)
+{
+    float distancia;
+
+    distancia = sqrt(pow((p->x - c->centro.x), 2) + pow((p->y - c->centro.y), 2));
+
+    if(distancia <= c->raio)
+        return 1;
+    else
+        return 0;
 }
 
 int main()
@@ -86,4 +89,5 @@ int main()
 
     leInstancias(&ponto1, &circ1);
     mostraInstancias(&ponto1, &circ1);
+    printf("\n%d\n", ehInterno(&ponto1, &circ1));
 }
